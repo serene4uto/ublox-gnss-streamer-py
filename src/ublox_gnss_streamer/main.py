ed2cf11e95e4edfae9262e193858c509a3880255
Zeki <nguyenhatrung411@gmail.com>
2025-05-23 07:42:23 +0000

    Add initial devcontainer setup, logger utility, and requirements file

---
+++ src/ublox_gnss_streamer/main.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(description="ublox_gnss_streamer")
+    
+    return parser.parse_args(argv)
+
+def main(argv=None):
+    
+    pass
+
+if __name__ == "__main__":
+    main()
