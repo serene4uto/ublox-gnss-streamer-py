c456ba28d8804ff3c3361253302851c3af6c7478
Zeki <nguyenhatrung411@gmail.com>
2025-05-23 12:57:18 +0000

    Update .gitignore and enhance build script to include additional dependencies and improve binary build process

--- scripts/build-bin.sh
+++ scripts/build-bin.sh
@@ -7,6 +7,28 @@ WORKSPACE_ROOT=$(readlink -f "$CURRENT_DIR/..")
 install_pkg() {
     sudo apt update
     sudo apt-get install -y \
-        patchelf
+        patchelf \
+        python3 \
+        python3-pip
+
+    pip install -r ${WORKSPACE_ROOT}/requirements.txt
 }
-build_bin()
+
+build_bin() {
+    python3 -m nuitka \
+        --standalone \
+        --onefile \
+        --output-dir="$WORKSPACE_ROOT/bin" \
+        ${WORKSPACE_ROOT}/ublox_gnss_streamer/main.py
+}
+
+
+# main entry
+install_pkg
+build_bin
+echo "Build completed. The binary is located in the bin directory."
+
+
+
+
+
