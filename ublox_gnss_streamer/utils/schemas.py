a02ea9f233c6c9cde1ed118a3c1a83977320e5af
Zeki <nguyenhatrung411@gmail.com>
2025-07-21 07:35:54 +0000

    Refactor GnssDataSchema: move type field to the top and comment out Literal options

--- ublox_gnss_streamer/utils/schemas.py
+++ ublox_gnss_streamer/utils/schemas.py
@@ -7,12 +7,13 @@ class GnssDataSchema(BaseModel):
     gnss_time: str
     lat: float
     lon: float
+    type: str
     # alt: float
-    type: Literal[
-        'extrapolated',     # Extrapolated data, not a real GNSS fix
-        'no-fix',           # No GNSS fix available
-        'no-rtk',           # Standard GNSS fix, no RTK
-        'float-rtk',        # RTK float solution
-        'fixed-rtk',        # RTK fixed solution
-        'dead-reckoning'    # Dead reckoning or combined solution
-    ]
+    # type: Literal[
+    #     'extrapolated',     # Extrapolated data, not a real GNSS fix
+    #     'no-fix',           # No GNSS fix available
+    #     'no-rtk',           # Standard GNSS fix, no RTK
+    #     'float-rtk',        # RTK float solution
+    #     'fixed-rtk',        # RTK fixed solution
+    #     'dead-reckoning'    # Dead reckoning or combined solution
+    # ]
